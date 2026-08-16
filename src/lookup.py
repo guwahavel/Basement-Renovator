40723d4c1767483cf9cc8c643dcfa8512f5b7e02
guwahavel <andrewangaroni@outlook.com>
2026-08-15 21:59:22 -0500

    Add second check for if content-repentogon/entities2.xml exists

--- src/lookup.py
+++ src/lookup.py
@@ -1429,6 +1429,14 @@ class MainLookup:
                     except ET.ParseError as e:
                         printf(f'ERROR parsing entities2 xml for mod "{modName}": {e}')
                         return
+                else:
+                    entities2Path = os.path.join(modPath, "content-repentogon/entities2.xml")
+                    try:
+                        self.entities2root = ET.parse(entities2Path).getroot()
+                    except ET.ParseError as e:
+                        printf(f'ERROR parsing entities2 xml for mod "{modName}": {e}')
+                        return
+
 
     def __init__(self, version, verbose):
         self.basemod = self.ModConfig()
